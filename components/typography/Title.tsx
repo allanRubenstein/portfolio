46bc0c885e7767e99596095c8a58a7e4060ad254
Allan Rubenstein <allan.rubenstein@willowtreeapps.com>
2021-08-18 14:39:28 -0400

    updating title component

--- components/typography/Title.tsx
+++ components/typography/Title.tsx
@@ -1,22 +1,29 @@
-import styled, { css, FlattenSimpleInterpolation } from 'styled-components';
+import styled from 'styled-components';
 import DynamicComponent from './DynamicComponent';
+import { css, FlattenSimpleInterpolation } from 'styled-components';
+import { TypographyProps } from './types';
 
 export const TitleStyle = (
-  size: number,
-  desktopSize?: number,
+  props: TypographyProps,
 ): FlattenSimpleInterpolation => css`
   font-family: 'Arial', 'sans-serif';
-  line-height: 1.92;
-  font-size: ${size}rem;
+  line-height: 1.2;
+  font-size: ${props.size}rem;
+  text-transform: uppercase;
+  font-family: 'Open Sans Condensed', sans-serif;
 
   /* TODO: come up with breakpoints */
   @media (min-width: 600px) {
-    ${desktopSize ? `font-size: ${desktopSize}rem` : ''}
+    ${props.desktopSize ? `font-size: ${props.desktopSize}rem` : ''}
   }
 `;
 
-const TestTitle = styled(DynamicComponent)`
-  ${(props) => TitleStyle(props.size, props.desktopSize)}
+// styled() constructor extends/inherits the styling of DynamicComponent in this case
+export const Title = styled(DynamicComponent)`
+  ${(props) => TitleStyle({ size: props.size, desktopSize: props.desktopSize })}
 `;
 
-export default TestTitle;
+export const TitleBold = styled(DynamicComponent)`
+  ${(props) => TitleStyle({ size: props.size, desktopSize: props.desktopSize })}
+  font-weight: 700;
+`;
--- components/typography/storybook/TitleBold.stories.tsx
+++ components/typography/storybook/TitleBold.stories.tsx
@@ -1,18 +1,18 @@
 import React from 'react';
 import { Story } from '@storybook/react';
-import { Title } from '../Title';
+import { TitleBold } from '../Title';
 import { DynamicComponentProps } from '../DynamicComponent';
 
 export default {
-  component: Title,
-  title: 'Components/Typography/Title',
+  component: TitleBold,
+  title: 'Components/Typography/TitleBold',
 };
 
 // TYPES IN STORYBOOK: This is how you do it
 // see https://storybook.js.org/tutorials/intro-to-storybook/react/en/simple-component/
 const Template: Story<DynamicComponentProps> = (
   args: DynamicComponentProps,
-) => <Title {...args} />;
+) => <TitleBold {...args} />;
 
 // Template.bind({}) is a standard JavaScript technique for making a copy of a function. We use this technique to allow each exported story to set its own properties, but use the same implementation.
 export const Default: Story<DynamicComponentProps> = Template.bind({});
